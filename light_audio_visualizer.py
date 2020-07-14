e6db6fea0b6a6d1a6d7c3c3e91e800082d4d2308
Raphael Beekmann <raphael.beekmann@acensi.fr>
2020-07-14 23:50:37 +0200

    feat: adapt audio_vizualizer

--- light_audio_visualizer.py
+++ light_audio_visualizer.py
@@ -1,18 +1,23 @@
 import collections
 import colorsys
-import time
-from threading import *
+import sys
 
 import numpy as np
 import pyaudio
+# import pyqtgraph as pg
+# from pyqtgraph.Qt import QtGui, QtCore
 
+import tkinter as tk
 from microphone_recorder import MicrophoneRecorder
 
 
-class AudioVisualizer(Thread):
-  def __init__(self, event):
-    Thread.__init__(self)
-    self.stopped = event
+class AudioVisualizerUI(tk.Frame):
+  def __init__(self, master=None):
+    super().__init__(master)
+    self.master = master
+    self.pack()
+
+
 
     self.FORMAT = pyaudio.paInt16
     self.CHANNELS = 1
@@ -20,21 +25,19 @@ class AudioVisualizer(Thread):
     self.CHUNKSIZE = 1024
     self.N_FFT = 2048
 
+    self.recorder = MicrophoneRecorder(sample_rate=self.RATE, chunksize=self.CHUNKSIZE)
+    self.recorder.start()
+
+    self.x = np.arange(0, 2 * self.CHUNKSIZE, 2)
+    self.f = np.linspace(1, self.RATE / 2, self.CHUNKSIZE)
+
     self.previous_hue = None
     self.hue_offset = 0
     self.previous_spectrum = collections.deque(maxlen=10)
 
-    self.recorder = MicrophoneRecorder(sample_rate=self.RATE, chunksize=self.CHUNKSIZE)
-    self.recorder.start()
-
-  def run(self):
-    while not self.stopped.wait(0.1):
-      self.update()
-
-  def stop(self):
-    self.recorder.close()
-    self.stopped.set()
-    self.join()
+  def start(self):
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+      QtGui.QGuiApplication.instance().exec_()
 
   def update(self):
     frames = self.recorder.get_frames()
@@ -43,16 +46,30 @@ class AudioVisualizer(Thread):
     else:
       data = frames[-1]
 
-    if data.max() > 1:
-      self.get_spectrum_data(data)
+    self.set_waveform_data(data)
 
-  def get_spectrum_data(self, data):
+    if data.max() > 1:
+      self.set_spectrum_data(data)
+
+  def set_waveform_data(self, data):
+    # self.waveform_plot.setData(x=self.x, y=data)
+    pass
+
+  def set_spectrum_data(self, data):
     spectrum = np.fft.fft(np.hanning(data.size) * data, n=self.N_FFT)
+
+    # if len(self.previous_spectrum) == 10:
+    #   for i in range(self.spectrum_smooth_slider.value()):
+    #     spectrum = spectrum + (self.previous_spectrum[i] - spectrum) * (self.spectrum_smooth_offset_slider.value() / 10)
+
+    # self.previous_spectrum.appendleft(spectrum)
 
     self.get_bark_split(spectrum)
 
-    # spectrum_magnitude = np.sqrt(np.real(spectrum) ** 2 + np.imag(spectrum) ** 2)
-    # spectrum_magnitude = spectrum_magnitude[:self.CHUNKSIZE] * 2 / (128 * self.CHUNKSIZE)
+    spectrum_magnitude = np.sqrt(np.real(spectrum) ** 2 + np.imag(spectrum) ** 2)
+    spectrum_magnitude = spectrum_magnitude[:self.CHUNKSIZE] * 2 / (128 * self.CHUNKSIZE)
+
+    # self.spectrum_plot.setData(x=self.f, y=spectrum_magnitude)
 
   def get_bark_split(self, data):
     bark_scale = [0, 100, 200, 300, 400, 510, 630, 770, 920, 1080, 1270, 1480, 1720, 2000,
@@ -109,6 +126,8 @@ class AudioVisualizer(Thread):
 
     self.set_color(y)
 
+    # self.bark_spectrum_plot.setData(x=bark_scale, y=y)
+
   def set_color(self, data):
     blue = np.linalg.norm(data[:8]) * 0.8
     green = np.linalg.norm(data[8:16])
@@ -121,12 +140,38 @@ class AudioVisualizer(Thread):
     blue_ratio = blue / sum
 
     hue, saturation, value = colorsys.rgb_to_hsv(red_ratio, green_ratio, blue_ratio)
+
+    if self.previous_hue is None:
+      self.previous_hue = hue
+
+    hue = hue + (self.previous_hue - hue) * (self.hue_smooth_slider.value() / 10)
+    self.previous_hue = hue
+
+    # if self.rainbow_mode_checkbox.isChecked():
+      # hue = hue + (self.previous_hue - hue) * 0.5
+      # self.hue_offset += 0.005
+      # hue = (hue + self.hue_offset) % 1
+      # print(hue)
+    # print((self.slider.value() / 10))
+
     r = colorsys.hsv_to_rgb(hue, 1, 1)
-    print(r)
-    # print(hue)
-    print(hue, hex(int(hue * 255)))
+
+    # self.label_rgb.setText('███')
+    # self.label_rgb.setAttr('color', '%02x%02x%02x' % (int(r[0] * 255), int(r[1] * 255), int(r[2] * 255)))
+
+  def animation(self):
+    # timer = QtCore.QTimer()
+    # timer.timeout.connect(self.update)
+    # timer.start(10)
+
+    self.start()
+
 
 if __name__ == '__main__':
-  stopFlag = Event()
-  thread = AudioVisualizer(stopFlag)
-  thread.start()
+  root = tk.Tk()
+  audio_app = AudioVisualizerUI(master=root)
+
+  audio_app.master.title('Audio Visualizer')
+  audio_app.master.maxsize(1000, 1000)
+  audio_app.mainloop()
+  # audio_app.animation()
